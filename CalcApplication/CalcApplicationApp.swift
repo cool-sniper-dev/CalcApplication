12a67762f4fca89c112aca581e1c4a519bba1b3f
cool-sniper-dev <82809928+cool-sniper-dev@users.noreply.github.com>
2021-07-17 14:26:16 +0200

    Initial Commit

---
+++ CalcApplication/CalcApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcApplicationApp.swift
+//  CalcApplication
+//
+//  Created by IACD-Air-3 on 2021/07/17.
+//
+
+import SwiftUI
+
+@main
+struct CalcApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalcApplication/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalcApplication
+//
+//  Created by IACD-Air-3 on 2021/07/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
